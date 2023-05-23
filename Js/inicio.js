b78e8322f2c3d72fdfea45f9b9bc681acafc41dc
Phtessarolo <phtessarolo@gmail.com>
2023-05-23 20:57:10 -0300

    Aula 23-05

---
+++ Js/inicio.js
@@ -0,0 +1,111 @@
+// console.log("Hello World");
+// alert(2+2);
+// prompt("Qual seu nome?");
+
+
+// // Váriaveis
+// var carro;
+// var ano;
+// console.log(carro, ano);
+// carro = prompt("Qual o carro?");
+// ano = prompt("Qual ano?");
+// alert(carro + " " + ano);
+// document.write(carro)
+
+// const n1 = 10; //inteiro    
+// const n2 = "12"; // String
+// const n3 = 20.5;    // Número decimal
+// const n4 = 30; // Inteiro
+// const maiotem31dias = true;
+
+// console.log(n1, n2, n3, n4, maiotem31dias);
+
+//Exercicio 01
+// let meuLog = "Meu primeiro log"
+// console.log(meuLog)
+
+// //Exercicio 02
+// let meuAlerta = "Meu primeiro alerta"
+// alert(meuAlerta)
+
+//Exercicio 03
+
+// let numero = prompt("Digite um número: ");
+// let numero2 = prompt("Digite outro numero: ");
+
+// let resultado = parseInt(numero) + parseInt(numero2);
+// console.log(resultado);
+// Array 
+// for(let i=1; i<10; i++){
+//     resultao = prompt("informe o " + i + "° ")
+// }
+
+// let n1 = 10;
+// let n2 = 5;
+
+// document.write("Soma: ", n1+n2);
+// document.write("Multiplicação: ", n1*n2, "\n");
+// document.write("Divisão: ", n1/n2, "\n");
+// document.write("Subtração: ", n1-n2);
+
+// const idade = 18;
+// const idadeMinimaParaDirigir = 18;
+// const idadeMaxima = 100;
+// const podeDirigir = idade >= idadeMinimaParaDirigir && idade <= idadeMaxima;
+// const naoPodeDirigir = idade < idadeMinimaParaDirigir || idade > idadeMaxima;
+
+// console.log("Pode Dirigir:", podeDirigir);
+// console.log("Não pode dirigir: ", naoPodeDirigir);
+// console.log("Não pode dirigir? (negação)", !podeDirigir);
+// console.log("Pode dirigir (comparação)", naoPodeDirigir != true);
+
+// let num = []
+
+
+// for(let i=0;i<2;i++){
+//    num[i] =  prompt(`Qual o valor do ${i+1} ° numero`);
+//    parsefloat(num[i]);
+//    let soma = soma + num[i];
+    
+// // }
+
+
+//     console.log(soma/3)
+
+// Exercicio 4
+
+// let salario = prompt("Informe o seu sálario: ");
+// salario = parseFloat(salario);
+
+// salario = salario*1.15;
+// salario = salario - salario*0.01;
+// alert("Seu novo salário: " + salario)
+
+// Exercicio 06
+
+let a, b, resultado;
+a = parseFloat(prompt("Qual o valor de A: "))
+b = parseFloat(prompt("Qual o valor de B: "))
+
+if(a==b){
+    resultado = a+b;
+    alert("Seu Resultado é: " + resultado);
+}
+else{
+    resultado = a*b;
+    alert("Seu Resultado é: " + resultado);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
